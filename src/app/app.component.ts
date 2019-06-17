96bd40aa5197eb96099c00eb6b5e316ee58757fd
SerArt <n7412440@gmail.com>
2019-06-17 13:19:59 +0300

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
- 
+  userName :string= '';
+
+  resetUser(){
+    console.log(this.userName)
+    this.userName = '';
+  }
 }
