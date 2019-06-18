69339c64a36930f12b082b8f228328584d6285cf
SerArt <n7412440@gmail.com>
2019-06-18 14:45:14 +0300

    completed solution

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  showSecret:boolean = false;
+  log = [];
+
+  onToggleDetails(){
+
+      this.showSecret = !this.showSecret;
+      this.log.push(new Date());
+
+  }
  
 }
