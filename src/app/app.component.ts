1545fa2d62cffa5e780366e3a0e1a2fdb0a95c1d
SerArt <n7412440@gmail.com>
2019-06-18 20:14:45 +0300

    wrap up /

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,14 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  showSecret:boolean = false;
-  log = [];
 
-  onToggleDetails(){
-
-      this.showSecret = !this.showSecret;
-      this.log.push(new Date());
-
-  }
  
 }
